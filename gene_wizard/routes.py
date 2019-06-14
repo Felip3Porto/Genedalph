0e67488b1bcb73b26637370c28fcc5f7d6d42179
Felip3Porto <felipewporto@gmail.com>
2019-06-14 08:35:08 -0400

    reducing size of queries

--- gene_wizard/routes.py
+++ gene_wizard/routes.py
@@ -1,11 +1,18 @@
 # if you want to import a model you need to keep in under the db instance, because it won't know until then
-# need to treat the rest of the blog like a package 
-from flask import Flask, render_template, send_from_directory, send_file, url_for, flash, redirect, request
+# need to treat the rest of gene_wizard like a package 
+from flask import Flask, render_template, send_from_directory, send_file, url_for, flash, redirect, request, Markup
 from gene_wizard import app, mysql
-#from gene_wizard.forms import 
-#from gene_wizard.models import 
 
-
+# use a custom agent of the Gprofiler API
+from gprofiler import GProfiler
+gp = GProfiler(
+    user_agent='Genedalph', #thanks Uku
+    return_dataframe=True, #return pandas dataframe or plain python structures    
+)
+# for the graphing to be sent
+import math
+import plotly.graph_objs as go
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 import pandas as pd
 
 #because they are in the package now ya gotta have the dot before your own created objects
@@ -61,7 +68,7 @@ def home():
 # def about():
 #     return render_template('about.html', title = 'About')
 
-@app.route('/gProfilerTest')
+@app.route('/gProfilerTest', methods=['GET', 'POST'])
 def gProfilerTest():
 
     conn = mysql.connect()
@@ -69,14 +76,46 @@ def gProfilerTest():
     try: 
         cursor.execute("SELECT DISTINCT e.ENSG_id FROM DEG_lncRNA_roster d, Expression e WHERE d.roster_id = e.DEG_lncRNA_roster_roster_id AND d.lncRNA_name = 'MANCR' AND e.log2FoldChange < 0;") 
         genes_down = cursor.fetchall()
-        
+        query_down=[i[0] for i in genes_down]
+        # seems like genes_down etc are very large even on the API side to handle
+        # makina a stringent separate query can get the most graph possible 
         cursor.execute("SELECT DISTINCT e.gene_symbol FROM DEG_lncRNA_roster d, Expression e WHERE d.roster_id = e.DEG_lncRNA_roster_roster_id AND d.lncRNA_name = 'MANCR' AND e.log2FoldChange > 0;") 
         genes_up = cursor.fetchall()
+        # needs to be done before the cursor close
+        # cursor object is not like a list at all 
+        query_up=[i[0] for i in genes_up]
         
     finally: 
         cursor.close()
+    print(query_down)
+    print(type(query_down))
+    print(len(query_down))
+    #query the upregulated and downregulated genes 
+    sources = ["GO:MF","GO:CC","GO:BP","KEGG","REAC","WP"]
+    # tricky cursor needs to be put in a python list 
 
-    return render_template('gProfilerTest.html', genes_down=genes_down, genes_up=genes_up)
+    # GET dataframes of pathways 
+    
+    # GOSTup = gp.profile(organism='hsapiens', query = query_up, sources = sources, no_evidences=False) 
+    # GOSTdown = gp.profile(organism='hsapiens', query = query_down, sources = sources, no_evidences=False) 
+    # get lists for plotting 
+    # 3 things GOids, -log(adjP), and terms
+    # xGOtermsUp = GOSTup.native.to_list()
+    # # remember that pval is already corrected and the bar is -log(padj)
+    # listPadjUp = GOSTup.p_value.to_list()
+    # yPadjUp = []
+    # for e in listPadjUp:
+    #     yPadjUp.append(-math.log(e))
+    # GOtextUp = GOSTup.name.to_list()
+    # xGOtermsDown = GOSTdown.native.to_list()
+    # listPadjDown = GOSTdown.p_value.to_list()
+    # yPadjDown = []
+    # for i in listPadjDown:
+    #     yPadjDown.append(-math.log(i))
+    # GOtextDown = GOSTdown.name.to_list()
+        
+    # since it is a dict cursor try sending the df as a dict or json
+    return render_template('gProfilerTest.html', genes_down=genes_down, genes_up=genes_up) #, jsonify(result={"status": 200})
 
 @app.route('/test')
 def test():
@@ -104,17 +143,4 @@ def send_image(filename):
     # filename = url_for(filename)
     return send_file(filename, mimetype='image/png')
     # return send_from_directory("Data" , filename)
-
-@app.route("/Data", methods=['GET', 'POST']) 
-def results():
-    form = LoginForm()
-    if form.validate_on_submit():
-
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('you have been logged in!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
-
-    return render_template('login.html', title = 'Login', form=form)
     
