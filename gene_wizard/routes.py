0a7fe0029b42488a442f3b260a21652b76c59956
Felip3Porto <felipewporto@gmail.com>
2019-07-09 16:46:46 -0400

    working cell type specific

--- gene_wizard/routes.py
+++ gene_wizard/routes.py
@@ -49,32 +49,30 @@ def home():
 
         lncRNA_name , cell_line = comma_sep.split(',', 1)
 
-        print(lncRNA_name)
-        print(cell_line)
         
         #make a cursor to use to connect to mysql
         cursor = mysql.connect().cursor()
         
         try: 
             # expression data 
-            cursor.execute("SELECT e.ENSG_id, e.gene_symbol, ROUND(e.baseMean, 3), ROUND(e.log2FoldChange, 3), ROUND(e.lfcSE, 3), ROUND(e.pvalue, 3) , ROUND(e.stat, 3), ROUND(e.padj, 3) FROM Expression e, DEG_lncRNA_roster d WHERE d.roster_id = e.DEG_lncRNA_roster_roster_id and d.lncRNA_name = '" + lncRNA_name + "';")
+            cursor.execute("SELECT e.ENSG_id, e.gene_symbol, ROUND(e.baseMean, 3), ROUND(e.log2FoldChange, 3), ROUND(e.lfcSE, 3), ROUND(e.pvalue, 3) , ROUND(e.stat, 3), ROUND(e.padj, 3) FROM Expression e, DEG_lncRNA_roster d WHERE d.roster_id = e.DEG_lncRNA_roster_roster_id and d.lncRNA_name = '" + lncRNA_name + "' and d.cell_line = '" + cell_line + "';")
             expression_data = cursor.fetchall()
 
             # cloud genes down 
-            cursor.execute("SELECT DISTINCT e.gene_symbol FROM DEG_lncRNA_roster d, Expression e WHERE d.roster_id = e.DEG_lncRNA_roster_roster_id AND d.lncRNA_name = '" + lncRNA_name + "' AND e.log2FoldChange < 0;") 
+            cursor.execute("SELECT DISTINCT e.gene_symbol FROM DEG_lncRNA_roster d, Expression e WHERE d.roster_id = e.DEG_lncRNA_roster_roster_id AND d.lncRNA_name = '" + lncRNA_name + "' AND d.cell_line = '" + cell_line + "' AND e.log2FoldChange < 0;") 
             genes_down = cursor.fetchall()
             # query genes down regulated 
-            cursor.execute("SELECT DISTINCT e.gene_symbol FROM DEG_lncRNA_roster d, Expression e WHERE d.roster_id = e.DEG_lncRNA_roster_roster_id AND d.lncRNA_name = '" + lncRNA_name + "' AND e.log2FoldChange < 0 AND e.padj < .3;") 
+            cursor.execute("SELECT DISTINCT e.gene_symbol FROM DEG_lncRNA_roster d, Expression e WHERE d.roster_id = e.DEG_lncRNA_roster_roster_id AND d.lncRNA_name = '" + lncRNA_name + "' AND d.cell_line = '" + cell_line + "' AND e.log2FoldChange < 0 AND e.padj < .3;") 
             GOSTgenes_down = cursor.fetchall()
             query_down = [i[0] for i in GOSTgenes_down]
             # seems like genes_down etc are very large even on the API side to handle
             # makina a stringent separate query can get the most graph possible 
 
             # cloud genes up regulated 
-            cursor.execute("SELECT DISTINCT e.gene_symbol FROM DEG_lncRNA_roster d, Expression e WHERE d.roster_id = e.DEG_lncRNA_roster_roster_id AND d.lncRNA_name = '" + lncRNA_name + "' AND e.log2FoldChange > 0;") 
+            cursor.execute("SELECT DISTINCT e.gene_symbol FROM DEG_lncRNA_roster d, Expression e WHERE d.roster_id = e.DEG_lncRNA_roster_roster_id AND d.lncRNA_name = '" + lncRNA_name + "' AND d.cell_line = '" + cell_line + "' AND e.log2FoldChange > 0;") 
             genes_up = cursor.fetchall()
             # query genes up regulated 
-            cursor.execute("SELECT DISTINCT e.gene_symbol FROM DEG_lncRNA_roster d, Expression e WHERE d.roster_id = e.DEG_lncRNA_roster_roster_id AND d.lncRNA_name = '" + lncRNA_name + "' AND e.log2FoldChange > 0 AND e.padj < .3;") 
+            cursor.execute("SELECT DISTINCT e.gene_symbol FROM DEG_lncRNA_roster d, Expression e WHERE d.roster_id = e.DEG_lncRNA_roster_roster_id AND d.lncRNA_name = '" + lncRNA_name + "' AND d.cell_line = '" + cell_line + "' AND e.log2FoldChange > 0 AND e.padj < .3;") 
             GOSTgenes_up = cursor.fetchall()
             # needs to be done before the cursor close
             # cursor object is not like a list at all 
@@ -134,7 +132,7 @@ def home():
                 yPadjDown.append(-math.log(i))
             GOtextDown = GOSTdown.name.to_list()
 
-            dynamic_title = "Top Significant Differentially Expressed functions after " + lncRNA_name + "'s knockdown"
+            dynamic_title = "Top Significant Differentially Expressed functions after " + lncRNA_name + "'s knockdown in the " + cell_line + " cell line"
 
             ## plotly time 
             traceUp = go.Bar(
@@ -172,7 +170,7 @@ def home():
             fig = go.Figure(data=data, layout=layout)
             div_holder = plot(fig, output_type='div',  filename='straight_bars')
 
-            return render_template('newResults.html', expression_data=expression_data, lncRNA_name=lncRNA_name,  genes_down=genes_down, genes_up=genes_up, div_holder=Markup(div_holder))
+            return render_template('newResults.html', expression_data=expression_data, lncRNA_name=lncRNA_name, cell_line=cell_line, genes_down=genes_down, genes_up=genes_up, div_holder=Markup(div_holder))
             
         cursor.close()
 
@@ -184,104 +182,6 @@ def home():
 # def about():
 #     return render_template('about.html', title = 'About')
 
-@app.route('/gProfilerTest', methods=['GET', 'POST'])
-def gProfilerTest():
-
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    try: 
-        # cloud genes down 
-        cursor.execute("SELECT DISTINCT e.gene_symbol FROM DEG_lncRNA_roster d, Expression e WHERE d.roster_id = e.DEG_lncRNA_roster_roster_id AND d.lncRNA_name = 'MANCR' AND e.log2FoldChange < 0;") 
-        genes_down = cursor.fetchall()
-        # query genes down regulated 
-        cursor.execute("SELECT DISTINCT e.gene_symbol FROM DEG_lncRNA_roster d, Expression e WHERE d.roster_id = e.DEG_lncRNA_roster_roster_id AND d.lncRNA_name = 'MANCR' AND e.log2FoldChange < 0 AND e.padj < .3;") 
-        GOSTgenes_down = cursor.fetchall()
-        query_down = [i[0] for i in GOSTgenes_down]
-        # seems like genes_down etc are very large even on the API side to handle
-        # makina a stringent separate query can get the most graph possible 
-        
-        # cloud genes up regulated 
-        cursor.execute("SELECT DISTINCT e.gene_symbol FROM DEG_lncRNA_roster d, Expression e WHERE d.roster_id = e.DEG_lncRNA_roster_roster_id AND d.lncRNA_name = 'MANCR' AND e.log2FoldChange > 0;") 
-        genes_up = cursor.fetchall()
-        # query genes up regulated 
-        cursor.execute("SELECT DISTINCT e.gene_symbol FROM DEG_lncRNA_roster d, Expression e WHERE d.roster_id = e.DEG_lncRNA_roster_roster_id AND d.lncRNA_name = 'MANCR' AND e.log2FoldChange > 0 AND e.padj < .3;") 
-        GOSTgenes_up = cursor.fetchall()
-        # needs to be done before the cursor close
-        # cursor object is not like a list at all 
-        query_up=[i[0] for i in GOSTgenes_up]
-        
-    finally: 
-        cursor.close()
-    
-    print(len(query_up))
-    #query the upregulated and downregulated genes 
-    sources = ["GO:MF","GO:CC","GO:BP","KEGG","REAC","WP", "HP"]
-    # tricky cursor needs to be put in a python list 
-
-    # even after a stringent filters we are capped at 80   
-    if len(query_down) > 70:
-        query_down = query_down[:70]
-    if len(query_up) > 70:
-        query_up = query_up[:70]
-
-    # GET dataframes of pathways 
-    GOSTup = gp.profile(organism='hsapiens', query = query_up, sources = sources, no_evidences=False, user_threshold = .5) 
-    GOSTdown = gp.profile(organism='hsapiens', query = query_down, sources = sources, no_evidences=False, user_threshold = .5) 
-    # get lists for plotting 
-    # 3 things GOids, -log(adjP), and terms
-    xGOtermsUp = GOSTup.native.to_list()
-    # remember that pval is already corrected and the bar is -log(padj)
-    listPadjUp = GOSTup.p_value.to_list()
-    yPadjUp = []
-    for e in listPadjUp:
-        yPadjUp.append(-math.log(e))
-    GOtextUp = GOSTup.name.to_list()
-    xGOtermsDown = GOSTdown.native.to_list()
-    listPadjDown = GOSTdown.p_value.to_list()
-    yPadjDown = []
-    for i in listPadjDown:
-        yPadjDown.append(-math.log(i))
-    GOtextDown = GOSTdown.name.to_list()
-
-    ## plotly time 
-    traceUp = go.Bar(
-        x=xGOtermsUp,
-        y=yPadjUp,
-        text=GOtextUp,
-        name='Significantly Up Regulated Genes',
-        marker=dict(color='rgb(158,202,225)',
-        line=dict(
-            color='rgb(8,48,107)',
-            width=1.5,)
-            ),
-            opacity=0.6
-        )
-    traceDown = go.Bar(
-        x=xGOtermsDown,
-        y=yPadjDown,
-        text=GOtextDown,
-        name='Significantly Down Regulated Genes',
-        marker=dict(color='rgb(214,39,40)',
-        line=dict(
-            color='rgb(247,182,210)',
-            width=1.5,)
-            ),
-            opacity=0.6
-        )
-    # keep the meat in trace 0 
-    
-    # try to unify 
-    data = [traceUp, traceDown]
-    layout= go.Layout(
-        title='Significant Differentially Expressed functions after a lncRNA knockdown',
-        yaxis=dict(title = '-log(adjusted p-value)')
-        )
-    fig = go.Figure(data=data, layout=layout)
-    div_holder = plot(fig, output_type='div',  filename='straight_bars') 
-
-       
-    # since it is a dict cursor try sending the df as a dict or json
-    return render_template('gProfilerTest.html', genes_down=genes_down, genes_up=genes_up, div_holder=Markup(div_holder)) #, jsonify(result={"status": 200})
 
 @app.route("/report/<filename>")
 def send_image(filename):
